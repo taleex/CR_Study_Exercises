99a64eec3ecb9fcd7bbc24a7a2f39466aafc4243
Taleex <matosjax@gmail.com>
2026-06-09 10:14:12 +0100

    Week 2 - exercise 1

--- Arrays/week2/day7-api.js
+++ Arrays/week2/day7-api.js
@@ -1,1 +1,15 @@
-//Excercise 1 - call endpoint
+//Excercise 1 - call endpoint with real data
+
+async function getUsers() {
+
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+getUsers();
